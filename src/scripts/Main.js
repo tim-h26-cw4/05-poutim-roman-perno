cd787f5b4a99e20dc780323f5bbf0512493bcdec
romanperno <6230960@cegepmontpetit.ca>
2026-02-26 08:53:33 -0500

    Main.js débuté
    class main et init fait

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,4 +1,18 @@
-import Icons from "./utils/Icons.js";
+import Icons from './utils/Icons.js';
 
-// La ligne suivante devrait être au TOUT début du init() du Main
-Icons.load();
+class Main {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    Icons.load();
+
+    const chefs = document.querySelectorAll('[data-component="Chef"]');
+    for (let i = 0; i < chefs.length; i++) {
+      const chef = chefs[i];
+    }
+  }
+}
+
+new Main();
